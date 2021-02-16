0441e1e674dd2430b0dea7fcd92a84a1a5470536
Andrea Mestre <amestre89@gmail.com>
2021-02-16 13:39:38 +0100

    Get, doing Post router

--- routes/users.js
+++ routes/users.js
@@ -25,9 +25,56 @@ router.get("/:id", function (req, res, next) {
 //GET one user update
 router.get("/:id", function (req, res, next) {
 	const { id } = req.params;
-	db(`SELECT * FROM Updates WHERE profile_id=${id};`)
+	db(
+		`SELECT Profiles.firstname, Profiles.lastname FROM Profiles INNER JOIN Updates ON Profiles.id=Updates.profile_id WHERE Profiles.id=${id}`
+	)
 		.then((results) => res.send(results.data[0]))
 		.catch((err) => res.status(500).send(err));
 });
 
+//how to get the historical?
+
+//POST the client fills the InitialForm
+router.post("/", function (req, res, next) {
+	//your code here
+	const {
+		date,
+		id,
+		firstname,
+		lastname,
+		age,
+		email,
+		phone,
+		job,
+		jobHoursPerDay,
+		injuries,
+		nocieveSubstances,
+		suplements,
+		rest,
+		nightWakeUps,
+		sleepEnvironment,
+		alergies,
+		carbohydratesFeeling,
+		prevTrainings,
+		objectives,
+		availability,
+		numTrainingDays,
+		squat,
+		benchPress,
+		deadweight,
+		height,
+		fat,
+		kcal,
+		proteins,
+		G,
+		water,
+	} = req.body;
+	db(
+		`INSERT INTO Profile (date, id, firstname, lastname, age, email, phone, job, jobHoursPerDay, injuries, nocieveSubstances, suplements, rest, nightWakeUps, sleepEnvironment, alergies, carbohydratesFeeling, prevTrainings, objectives, availability, numTrainingDays, squat, benchPress, deadweight, height, fat, kcal, proteins, G, water) VALUES ("${date}", "${id}", "${firstname}", "${lastname}", "${age}", "${email}", "${phone}", "${job}", "${job}", "${jobHoursPerDay}", "${injuries}", "${nocieveSubstances}", "${suplements}", "${rest}", "${nightWakeUps}", "${sleepEnvironment}", "${alergies}"), "${carbohydratesFeeling}", "${prevTrainings}", "${objectives}", "${availability}", "${numTrainingDays}", "${squat}", "${benchPress}", "${deadweight}", "${height}", "${fat}", "${kcal}", "${proteins}", "${G}", "${water}";`
+	)
+		.then(() => {
+			res.send({ message: "El cuestionario se ha enviado correctamente" });
+		})
+		.catch((err) => res.status(500).send(err));
+});
 module.exports = router;
